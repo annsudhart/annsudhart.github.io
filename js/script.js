d937f8f32086c1a76a9cd81a25586e6497f076f4
annsudhart <sudharta.andrea@gmail.com>
2018-11-27 00:02:57 -0800

    fixed a bug disabling document.scroll event

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  console.log('running');
   if ( $(this).scrollTop() > $('.ham').height() ) {
     $('.ham').addClass('blue');
     $('.ham').removeClass('white');
@@ -6,35 +7,38 @@ $(document).ready(function() {
     $('.ham').addClass('white');
     $('.ham').removeClass('blue');
   }
-    
+
   $('#arrow').click(function() {
     $('html, body').animate({
       scrollTop: $("#container").offset().top
-    }, 800); 
+    }, 800);
   });
-  
+
   $('#internal-link li a').not('.hamburger').click(function() {
     var target = $(this).attr('href');
     $('html, body').animate({
       scrollTop: $(target).offset().top
-    }, 800); 
+    }, 800);
   });
-  
-  $(document).scroll(function() {
-    if ( $(this).scrollTop() > $('.ham').height() ) {
-      $('.ham').addClass('blue');
-      $('.ham').removeClass('white');
-    } else {
-      $('.ham').addClass('white');
-      $('.ham').removeClass('blue');
-    }
-  });
-  
+
   $('.hamburger').click(function() {
     $('#links').slideToggle(300);
     $('.top').toggleClass('rotate');
     $('.middle').toggleClass('rotate-back');
     $('.bottom').toggleClass('ham-hidden');
   });
-  
+
+  $('html, body').css('overflow-x', 'visible');
+});
+
+$(document).scroll(function() {
+  console.log('scrollTop');
+  if ( $(this).scrollTop() > $('.ham').height() ) {
+    console.log('turning blue');
+    $('.ham').addClass('blue');
+    $('.ham').removeClass('white');
+  } else {
+    $('.ham').addClass('white');
+    $('.ham').removeClass('blue');
+  }
 });
