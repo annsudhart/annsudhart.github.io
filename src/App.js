b0afe666a9c6ca4d6a80dc59d39f91904cca1785
annsudhart <asudhart@ucsd.edu>
2019-07-06 15:56:43 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar/>
+      <div className="splash"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand" href="">
+                <a className="navbar-brand">Andrea Sudharta</a>
+                <ul>
+                    <li className="navbar-item">About</li>
+                    <li className="navbar-item">Projects</li>
+                    <li className="navbar-item">Resume</li>
+                    <li className="navbar-item">GitHub</li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
