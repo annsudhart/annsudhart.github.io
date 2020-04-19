8e1c4319e37f1f4b5a21f04da3be54416a276928
Andrea Sudharta <asudhart@ucsd.edu>
2020-04-19 15:05:54 -0700

    cleaned folder structure and mobile styling for cards

---
+++ public/parallax.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+    let parent = document.getElementById('bkgd-img-splash');
+    let children = parent.getElementsByTagName('div');
+    for(let i = 0; i < children.length; i++) {
+      var offsetVal = window.pageYOffset * Math.sqrt((i+1) / children.length);
+      //if(offsetVal <= 85) {
+        children[i].style.transform = 'translateY(-' + (offsetVal) + 'px)';
+      //}
+    }
+}, false)
