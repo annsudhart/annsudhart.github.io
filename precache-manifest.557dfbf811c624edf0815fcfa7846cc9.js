fc77a722c81adcb3d1f321621a85b99b8dc36fb0
annsudhart <asudhart@ucsd.edu>
2020-05-18 01:07:34 -0700

    Updates

---
+++ precache-manifest.557dfbf811c624edf0815fcfa7846cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e5482dbc5e35315415710b087fa4ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae87025035e2ec635e5",
+    "url": "/static/css/main.6893fcea.chunk.css"
+  },
+  {
+    "revision": "2d3d9cb5d4f6f185d98a",
+    "url": "/static/js/2.4373ddc9.chunk.js"
+  },
+  {
+    "revision": "baf0dffd44398109a6c5d3770f0314f2",
+    "url": "/static/js/2.4373ddc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae87025035e2ec635e5",
+    "url": "/static/js/main.4c650b31.chunk.js"
+  },
+  {
+    "revision": "db300584e9b2323ba6e0",
+    "url": "/static/js/runtime-main.5b5eb2df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app/precache-manifest.5dfe25b5fc4f13c2fb4da4b7f0ea79d6.js"
+  "/precache-manifest.557dfbf811c624edf0815fcfa7846cc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
