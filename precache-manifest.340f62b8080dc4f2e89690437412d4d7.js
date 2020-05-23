439316cce889856f62e7d318777c00c46ec6c180
annsudhart <asudhart@ucsd.edu>
2020-05-22 23:50:45 -0700

    Updates

--- precache-manifest.340f62b8080dc4f2e89690437412d4d7.js
+++ precache-manifest.340f62b8080dc4f2e89690437412d4d7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d4e5482dbc5e35315415710b087fa4ba",
+    "revision": "29d0fba4af1a29261f45441abda4e1da",
     "url": "/index.html"
   },
   {
-    "revision": "aae87025035e2ec635e5",
+    "revision": "be04a38404977fd75d26",
     "url": "/static/css/main.6893fcea.chunk.css"
   },
   {
-    "revision": "2d3d9cb5d4f6f185d98a",
-    "url": "/static/js/2.4373ddc9.chunk.js"
+    "revision": "f8819e3599a3b1d7a377",
+    "url": "/static/js/2.bac9a795.chunk.js"
   },
   {
     "revision": "baf0dffd44398109a6c5d3770f0314f2",
-    "url": "/static/js/2.4373ddc9.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.bac9a795.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aae87025035e2ec635e5",
-    "url": "/static/js/main.4c650b31.chunk.js"
+    "revision": "be04a38404977fd75d26",
+    "url": "/static/js/main.0bd2b7db.chunk.js"
   },
   {
     "revision": "db300584e9b2323ba6e0",
